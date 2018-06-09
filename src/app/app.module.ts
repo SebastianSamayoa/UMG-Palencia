ea8175592de19157ae9ffd90db050125ba3661b2
SebastianSamayoa <samayoa_jhoan@hotmail.com>
2018-06-08 21:47:40 -0600

    implementacion de rutas parte 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { SlidesComponent } from './slides/slides.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
 import { FooterComponent } from './footer/footer.component';
 import { BodyComponent } from './body/body.component';
+import { BlogComponent } from './blog/blog.component';
+import { APP_ROUTES } from './app.routes';
+import { Blog1Component } from './blog1/blog1.component';
 
 
 @NgModule({
@@ -15,10 +17,13 @@ import { BodyComponent } from './body/body.component';
     SlidesComponent,
     CabeceraComponent,
     FooterComponent,
-    BodyComponent
+    BodyComponent,
+    BlogComponent,
+    Blog1Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { RouterModule , Routes  } from '@angular/router';
+
+import { BlogComponent } from './blog/blog.component';
+import { Blog1Component } from './blog1/blog1.component';
+
+const appRoutes: Routes = [
+    { path: '', component: BlogComponent },
+    { path: 'blog#/', component: Blog1Component },
+    { path: '**', component: Blog1Component }
+// tslint:disable-next-line:eofline
+];
+
+// tslint:disable-next-line:eofline
+export const APP_ROUTES = RouterModule.forRoot( appRoutes, { useHash: true } );
